0c5ae9570b79f397ee91a129dabd61d470d89703
Rishi Raghu <rishiraghu22@yahoo.com>
2023-09-23 18:36:41 -0400

    Updating Ques 6

--- Submissions/002722344_Rishi_Raghu/Ques6_Sol.py
+++ Submissions/002722344_Rishi_Raghu/Ques6_Sol.py
@@ -32,91 +32,137 @@ def maintenance_path(graph, start):
     return path
 
 def safest_path(graph, start, end):
+    candidates = [(0, start, [])]
     visited = set()
-    queue = deque([(start, [start], 0)])  # Node, path, safety_sum
     
-    safest_route = None
-    max_safety = float('-inf')
-    
-    while queue:
-        current, path, safety_sum = queue.popleft()
-        if current == end:
-            if safety_sum > max_safety:
-                max_safety = safety_sum
-                safest_route = path
+    while candidates:
+        candidates.sort(reverse=True)  # Sort by safety, descending
+        safety_sum, current, path = candidates.pop(0)  # Get the safest route
+        
+        if current in visited:
             continue
-          
         visited.add(current)
+        
+        path = path + [current]
+        
+        if current == end:
+            return path
         
         for neighbor, safety in graph[current].items():
             if neighbor not in visited:
                 new_safety_sum = safety_sum + safety
-                queue.append((neighbor, path + [neighbor], new_safety_sum))
+                candidates.append((new_safety_sum, neighbor, path))
                 
-    return safest_route
+    return "Not possible"
 
-def find_loops(graph, start):
+def min_charging_stops(graph, start, end, stations):
     visited = set()
-    stack = []
-    
-    def dfs(current, stack):
-        if current in stack:
-            return stack[stack.index(current):]
-        
-        if current in visited:
-            return []
-        
-        visited.add(current)
-        stack.append(current)
-        
-        for neighbor in graph[current]:
-            loop = dfs(neighbor, stack)
-            if loop:
-                return loop
-        
-        stack.pop()
-        return []
-    
-    return dfs(start, stack)
+    queue = deque([(start, [start], 0)])  # Node, Path, Stops
 
+    while queue:
+        current_node, path, stops = queue.popleft()
+        visited.add(current_node)
 
+        # If we reach the destination, return the path
+        if current_node == end:
+            return path
 
+        for neighbor in graph[current_node]:
+            if neighbor not in visited:
+                new_stops = stops
+                # If there is a charging station at the neighbor, reset the stops counter
+                if neighbor in stations:
+                    new_stops += 1
+                # Add the neighbor node to the queue for future processing
+                queue.append((neighbor, path + [neighbor], new_stops))
+
+    # If a path is not found
+    return None
 
 # Test the function
 weighted_graph = {
-    'A': {'B': 3, 'C': 2},
-    'B': {'A': 3, 'D': 4},
-    'C': {'A': 2, 'D': 1},
-    'D': {'B': 4, 'C': 1}
+    'A': {'B': 5},
+    'B': {'A': 5, 'C': 5, 'D': 5},
+    'C': {'B': 5, 'E': 5},
+    'D': {'B': 5, 'E': 5},
+    'E': {'C': 5, 'D': 5}
 }
 
 # Example usage:
 
 graph = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D'],
-    'C': ['A', 'D'],
-    'D': ['B', 'C', 'E'],
-    'E': ['D', 'F', 'G'],
-    'F': ['E', 'G'],
-    'G': ['E', 'F']
+    'A': ['B', 'D'],
+    'B': ['C', 'E'],
+    'C': ['F'],
+    'D': ['E', 'G'],
+    'E': ['F', 'H'],
+    'F': ['I'],
+    'G': ['H'],
+    'H': ['I'],
+    'I': []
 }
 
-graph_new = {
-    'W': ['X', 'Z'],
-    'X': ['W', 'Y'],
-    'Y': ['Z', 'X'],
-    'Z': ['W', 'Y']
-}
+start = 'A'
+end = 'I'
+stations = ['B', 'D', 'F', 'H']
 
 
+
+result = shortest_path(graph, 'A', 'D')
+print("Shortest path:", result)
 
 result = maintenance_path(graph, 'A')
 print("Maintenance Path:", result)
 
-result = shortest_path(graph, 'A', 'H')
-print("Shortest path from A to H:", result)
+# print(safest_path(weighted_graph, 'A', 'E'))  # Output could be ['A', 'C', 'D', 'E']
+# print(safest_path(weighted_graph, 'A', 'D'))  # Output could be ['A', 'C', 'D']
+# print(safest_path(weighted_graph, 'A', 'B'))  # Output could be ['A', 'B']
 
-print(safest_path(weighted_graph, 'A', 'D'))  # Output could be ['A', 'B', 'D']
+result = min_charging_stops(graph, start, end, stations)
+print(result)
 
-print(find_loops(graph_new, 'W'))  # Output should be ['A', 'B', 'C']
+# graph1 = {
+#     'A': ['B', 'C'],
+#     'B': ['A', 'D', 'E'],
+#     'C': ['A', 'F', 'G'],
+#     'D': ['B', 'H'],
+#     'E': ['B', 'H'],
+#     'F': ['C', 'I'],
+#     'G': ['C', 'I'],
+#     'H': ['D', 'E', 'I'],
+#     'I': ['F', 'G', 'H']
+# }
+# stations1 = ['B', 'E', 'G', 'I']
+# start1 = 'A'
+# end1 = 'I'
+# path1, stops1 = min_charging_stops(graph1, start1, end1, stations1)
+# print(f"Test Case 1:\nPath: {path1}\nCharging Stops: {stops1}")
+
+# graph2 = {
+#     'A': ['B', 'D'],
+#     'B': ['A', 'C', 'E'],
+#     'C': ['B', 'E'],
+#     'D': ['A', 'E'],
+#     'E': ['B', 'C', 'D'],
+#     'F': ['E', 'G'],
+#     'G': ['F', 'H'],
+#     'H': ['G', 'I'],
+#     'I': ['H']
+# }
+# stations2 = ['C', 'F', 'H']
+# start2 = 'A'
+# end2 = 'I'
+# path2, stops2 = min_charging_stops(graph2, start2, end2, stations2)
+# print(f"Test Case 2:\nPath: {path2}\nCharging Stops: {stops2}")
+
+# graph3 = {
+#     'A': ['B'],
+#     'B': ['A', 'C'],
+#     'C': ['B', 'D'],
+#     'D': ['C']
+# }
+# stations3 = ['B']
+# start3 = 'A'
+# end3 = 'D'
+# path3, stops3 = min_charging_stops(graph3, start3, end3, stations3)
+# print(f"Test Case 3:\nPath: {path3}\nCharging Stops: {stops3}")
